9e178609ddc331cb113b952bd39ef4e9a33f0940
remus32 <matyas2303@gmail.com>
2016-02-07 16:01:39 +0100

    Update EventEmitter

--- scalajs/src/main/scala/io/atom/electron/EventEmitter.scala
+++ scalajs/src/main/scala/io/atom/electron/EventEmitter.scala
@@ -9,4 +9,7 @@ trait EventEmitter extends js.Any {
 
   def once(event: String, listener: js.Function0[Unit]): EventEmitter = js.native
 
+  def removeListener(channel:String, callback: js.Function0[Unit]): Unit = js.native
+
+  def removeAllListeners(channel:String): Unit = js.native
 }
